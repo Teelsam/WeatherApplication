f70da0a466c2a970801ff5232343838962017105
Sam Teel <sjkteel@hotmail.com>
2023-06-03 14:28:02 -0400

    town submit is saved to localstorage

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
+var inputEl = $('#subLoc');//content in inputEl
 var confirmBtn = $('#confirmBtn');
-var inputLocation = $('#inputLocal');
+var pickedLoc = $('#locCheck');
+var placeName = inputEl.val();
 
-confirmBtn.on('click', function () {
-    var typedLocal = inputLocation.val();
-    localStorage.setItem('typedLocal', typedLocal);
+
+confirmBtn.on('click', function (event) {
+    event.preventDefault();
+    placeName = inputEl.val();
+    console.log('placeName');
+    localStorage.setItem('placeName', placeName);
 
 });
+
+function printSelected() {
+    var town = localStorage.getItem('placeName', placeName);
+    console.log('town: ' + town);
+    pickedLoc.innerHTML = town;
+}
+printSelected();
