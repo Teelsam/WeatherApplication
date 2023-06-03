f90af6e61a575fcfd48df9825ad036ed6eac2e66
Sam Teel <sjkteel@hotmail.com>
2023-06-03 14:50:52 -0400

    prints chosen town

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-var inputEl = $('#subLoc');//content in inputEl
-var confirmBtn = $('#confirmBtn');
-var pickedLoc = $('#locCheck');
-var placeName = inputEl.val();
+$(function () {
+    var inputEl = $('#subLoc');//content in inputEl
+    var confirmBtn = $('#confirmBtn');
+    var placeName = inputEl.textContent;
+    var printName = $('#locCheck');
 
 
-confirmBtn.on('click', function (event) {
-    event.preventDefault();
-    placeName = inputEl.val();
-    console.log('placeName');
-    localStorage.setItem('placeName', placeName);
+    confirmBtn.on('click', function (event) {
+        event.preventDefault();
+        placeName = inputEl.val();
+        console.log('placeName: ' + placeName);
+        localStorage.setItem('placeName', placeName);
+    });
+    function printSelected() {
+        var town = localStorage.getItem('placeName', placeName);
+        console.log('town: ' + town);
+        printName.empty();
+        printName.text(town);
+
+    }
+    printSelected();
+
 
 });
-
-function printSelected() {
-    var town = localStorage.getItem('placeName', placeName);
-    console.log('town: ' + town);
-    pickedLoc.innerHTML = town;
-}
-printSelected();
