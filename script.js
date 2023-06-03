6d29bcc466bc97878707b8326abfdc1771ad9a84
Sam Teel <sjkteel@hotmail.com>
2023-06-03 11:49:18 -0400

    began script

---
+++ script.js
@@ -0,0 +1,9 @@
+var confirmBtn = $('#confirmLocal');
+var inputLocation = $('#inputLocal');
+
+confirmBtn.on('click', function () {
+    var btnEl = 
+    btnEl.val() = inputLocation.val();
+    // $('buttonContainer').append(btnEl);
+
+});
